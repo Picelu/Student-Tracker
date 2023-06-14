a2cdb37c300a23bab2457ad99d3a6d9fb617f986
msblee123 <62529126+Picelu@users.noreply.github.com>
2023-06-13 20:26:09 -0700

    placeholder

--- src/main/resources/static/JS/script.js
+++ src/main/resources/static/JS/script.js
@@ -52,28 +52,40 @@ function editUser(event, id) {
 
   const form = document.createElement("form");
 
-  const nameLabel = document.createElement("label");
-  nameLabel.textContent = "Name:";
   const nameInput = document.createElement("input");
   nameInput.type = "text";
   nameInput.name = "name";
-  nameInput.placeholder = "Enter name";
+  nameInput.placeholder = "Edit Name";
 
-  const emailLabel = document.createElement("label");
-  emailLabel.textContent = "Email:";
   const emailInput = document.createElement("input");
   emailInput.type = "email";
   emailInput.name = "email";
-  emailInput.placeholder = "Enter email";
+  emailInput.placeholder = "Edit Email";
+
+  const passwordInput = document.createElement("input");
+  passwordInput.type = "password";
+  passwordInput.name = "password";
+  passwordInput.placeholder = "Edit Password";
+
+  const gpaInput = document.createElement("input");
+  gpaInput.type = "float";
+  gpaInput.name = "gpa";
+  gpaInput.placeholder = "Edit GPA";
+
+  const heightInput = document.createElement("input");
+  heightInput.type = "number";
+  heightInput.name = "height";
+  heightInput.placeholder = "Edit Height";
 
   const submitButton = document.createElement("button");
   submitButton.type = "submit";
   submitButton.textContent = "Save";
 
-  form.appendChild(nameLabel);
   form.appendChild(nameInput);
-  form.appendChild(emailLabel);
   form.appendChild(emailInput);
+  form.appendChild(passwordInput);
+  form.appendChild(gpaInput);
+  form.appendChild(heightInput);
   form.appendChild(submitButton);
 
   form.addEventListener("submit", (event) => {
